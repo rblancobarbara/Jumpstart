a3095e3045d3ee2b913c3323c77499a0a1ee322f
Ricardo Blanco <rblancobarbara@yahoo.com.ar>
2018-01-26 14:03:25 -0500

    Final version.

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,6 +6,20 @@ $(document).ready(function() {
     arrows: false,
     draggable: false,
     pauseOnFocus: false,
-    pauseOnHover: false
+    pauseOnHover: false,
   });
+
+  var lastScrolled = 0;
+  $(window).scroll(function() {
+    var scrolled = $(this).scrollTop();
+    if (scrolled - lastScrolled > 50) {
+      var navbarHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navbarHeight}, 150);
+      lastScrolled = scrolled;
+      } else if (lastScrolled - scrolled > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrolled = scrolled;
+      }
+  });
+
 });
